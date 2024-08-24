c16535e1c437c6fc5fc8fd6c3fbeb7b1bbe8a615
baseplate77 <base8087@gmail.com>
2024-08-24 12:26:04 +0530

    fff

--- app/utils/get_profile_data.ts
+++ app/utils/get_profile_data.ts
@@ -1,15 +1,17 @@
+import puppeteer from "puppeteer";
 import { getReaLBrowser } from "./browerSetup";
 import delay from "./delay";
+import { response } from "express";
 
 export const getProfileData = async (userId: string, cb: any) => {
   const { page, browser } = await getReaLBrowser();
   let profileData: any;
+  const profileDetailAPI = `https://api.notjustanalytics.com/profile/ig/analyze/${userId}`;
 
   const handleResponse = async (response: any) => {
     const url = response.url() as string;
     const status = response.status();
     const type = response.request().resourceType();
-    const profileDetailAPI = `https://api.notjustanalytics.com/profile/ig/analyze/${userId}`;
 
     if (type === "xhr" || type === "fetch") {
       try {
@@ -41,23 +43,32 @@ export const getProfileData = async (userId: string, cb: any) => {
 
     try {
       await page.goto(`https://app.notjustanalytics.com/analysis/${userId}`, {
-        waitUntil: ["domcontentloaded", "networkidle2"],
-        timeout: 90000,
+        waitUntil: ["load"],
+        timeout: 60000,
       });
     } catch (error) {
-      console.log("Error in page navigation:", error);
+      console.log("Error in page navigation:");
     }
 
-    console.log("Page loaded successfully");
+    // const browser = puppeteer.launch();
+    // let page = await (await browser).newPage();
+    // await page.waitForRequest((response: any) => {
+    //   let r = response.url().includes(profileDetailAPI);
+    //   console.log("response :", r, "url :", response.url());
+
+    //   return r;
+    // });
+
+    console.log("Page loaded successfully :", profileData, "data");
   } catch (error) {
     console.error("Failed to load the page:", error);
   } finally {
     if (profileData === undefined) {
       console.log("Profile data not defined");
-      throw new Error("profile not found");
+      // throw new Error("profile not found");
     }
 
-    await browser.close();
+    // await browser.close();
     cb(profileData);
     console.log(userId, profileData.followers);
     await delay(1000);
