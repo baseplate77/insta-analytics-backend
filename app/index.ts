c451f4df63805c206b1c3b63e2939c2e6f600753
baseplate77 <base8087@gmail.com>
2024-08-30 23:12:38 +0530

    ffff

--- app/index.ts
+++ app/index.ts
@@ -19,7 +19,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
